9af7df3e82c358376e464f72fa80dfbd2c280964
christine <ctjoo11@gmail.com>
2023-05-05 18:09:11 -0700

    Add categories page and api

---
+++ pages/categories.js
@@ -0,0 +1,127 @@
+import Layout from "@/components/Layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { withSwal } from "react-sweetalert2";
+
+function Categories({ swal }) {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [name, setName] = useState("");
+  const [parentCategory, setParentCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+  function fetchCategories() {
+    axios.get("/api/categories").then((result) => {
+      setCategories(result.data);
+    });
+  }
+  async function saveCategory(e) {
+    e.preventDefault();
+    const data = { name, parentCategory };
+    if (selectedCategory) {
+      data._id = selectedCategory._id;
+      await axios.put("/api/categories", data);
+      setSelectedCategory(null);
+    } else {
+      await axios.post("/api/categories", data);
+    }
+    setName("");
+    fetchCategories();
+  }
+
+  function editCategory(category) {
+    setSelectedCategory(category);
+    setName(category.name);
+    setParentCategory(category.parent?._id);
+  }
+
+  function deleteCategory(category) {
+    swal
+      .fire({
+        title: "Are you sure?",
+        text: `Do you want to delete ${category.name}?`,
+        showCancelButton: true,
+        cancelButtonText: "Cancel",
+        confirmButtonText: "Yes, Delete!",
+        confirmButtonColor: "#d55",
+      })
+      .then(async (result) => {
+        if (result.isConfirmed) {
+          const { _id } = category;
+          await axios.delete("/api/categories?_id=" + _id);
+          fetchCategories();
+        }
+      });
+  }
+
+  return (
+    <Layout>
+      <h1>Categories</h1>
+      <label>
+        {selectedCategory
+          ? `Edit Category ${selectedCategory.name}`
+          : "Create New Category"}
+      </label>
+      <form onSubmit={saveCategory} className="flex gap-1">
+        <input
+          className="mb-0"
+          type="text"
+          placeholder="Category Name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <select
+          className="mb-0"
+          value={parentCategory}
+          onChange={(e) => setParentCategory(e.target.value)}
+        >
+          <option value="">No Parent Category</option>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <option key={category._id} value={category._id}>
+                {category.name}
+              </option>
+            ))}
+        </select>
+        <button type="submit" className="btn-primary py-1">
+          Save
+        </button>
+      </form>
+      <table className="basic mt-4">
+        <thead>
+          <tr>
+            <td>Category Name</td>
+            <td>Parent Category</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <tr key={category._id}>
+                <td>{category.name}</td>
+                <td>{category?.parent?.name}</td>
+                <td>
+                  <button
+                    onClick={() => editCategory(category)}
+                    className="btn-primary mr-1"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="btn-primary"
+                    onClick={() => deleteCategory(category)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+}
+
+export default withSwal(({ swal }, ref) => <Categories swal={swal} />);
